a667405aaa48134b2e7ce073db21407235cfc975
IBrickhouse <brickhoi@oregonstate.edu>
2022-02-17 17:15:36 -0800

    Fix Typo

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ class Generate_Puzzle:
     def draw_text(display, text, size, x, y):
         font = pygame.font.Font(font_name, size)
         text_surface = font.render(text, True, brown)
-        text_rec = text_surface.get_rect()
+        text_rect = text_surface.get_rect()
         text_rect.midtop = (x, y)
         gameDisplay.blit(text_surface, text_rect)
 
