b93c7a6e07ddbadfca856b30647987902c9c6a2b
IBrickhouse <brickhoi@oregonstate.edu>
2022-02-19 23:40:13 -0800

    Add Momo test photo and size function
    add Momo test photo and experimental test function

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ brown = (100,40,0)
 
 
 
-background = pygame.image.load('images/red.jpg')           #setting game background image
+background = pygame.image.load('images/momo.jpg')           #setting game background image
 background = pygame.transform.scale(background, (800, 600))
 
 font = pygame.font.Font(os.path.join(os.getcwd(), 'Comic Book.ttf'), 70)
@@ -340,4 +340,39 @@ while game_running :
 pygame.quit()
 
 
+# background image size getter (may more to seperate file in future)
+def get_image_size(fname):
+    '''Determine the image type of fhandle and return its size.
+    from draco'''
+    with open(fname, 'rb') as fhandle:
+        head = fhandle.read(24)
+        if len(head) != 24:
+            return
+        if imghdr.what(fname) == 'png':
+            check = struct.unpack('>i', head[4:8])[0]
+            if check != 0x0d0a1a0a:
+                return
+            width, height = struct.unpack('>ii', head[16:24])
+        elif imghdr.what(fname) == 'gif':
+            width, height = struct.unpack('<HH', head[6:10])
+        elif imghdr.what(fname) == 'jpeg':
+            try:
+                fhandle.seek(0) # Read 0xff next
+                size = 2
+                ftype = 0
+                while not 0xc0 <= ftype <= 0xcf:
+                    fhandle.seek(size, 1)
+                    byte = fhandle.read(1)
+                    while ord(byte) == 0xff:
+                        byte = fhandle.read(1)
+                    ftype = ord(byte)
+                    size = struct.unpack('>H', fhandle.read(2))[0] - 2
+                # We are at a SOFn block
+                fhandle.seek(1, 1)  # Skip `precision' byte.
+                height, width = struct.unpack('>HH', fhandle.read(4))
+            except Exception: #IGNORE:W0703
+                return
+        else:
+            return
+        return width, height
     
